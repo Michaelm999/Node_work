a7e3e37d1cb92c608ae70a5f201b78117682412a
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-12 11:42:23 -0700

    Tasks

--- d03/task-list/server.js
+++ d03/task-list/server.js
@@ -2,13 +2,34 @@ const mongoose = require('mongoose')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+
 const app = express()
 const PORT = 3000
-const mongoURL = 'mongodb://localhost/task-list'
+const mongoURL = 'mongodb://localhost/todo_spa'
+
+mongoose.connect(mongoURL, function(err) {
+  if (err) console.log(err)
+  console.log('Connected to mongo database:', mongoURL);
+})
+
+const TaskSchema = new mongoose.Schema({
+  body: String, completed: Boolean
+})
+const Task = mongoose.model('Task', TaskSchema)
+
+Task.create({body: 'play ball', completed: false})
 
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname})
 })
+
+app.get('/tasks', function(req, res){
+  Task.find({}, function(err, tasks){
+    if (err) console.log(err)
+      res.send(tasks)
+  })
+})
+
 
 
 app.listen(PORT, function(){'listening on port', PORT})
