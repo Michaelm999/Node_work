dd86bb3b9b8cd2a27272ae54d168c298d6e3948b
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-13 15:43:01 -0700

    The story so far

--- ajax_giphy_starter/index.js
+++ ajax_giphy_starter/index.js
@@ -1,9 +1,11 @@
 const
+  dotenv = require('dotenv').load(),
   express = require('express'),
   app = express(),
   logger = require('morgan'),
   bodyParser = require('body-parser'),
   port = 3000
+  request = require('request')
 
 app.set('view engine', 'ejs')
 
@@ -14,6 +16,18 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res) {
   res.render('search')
 })
+
+app.get('/search/:searchTerm', function(req, res) {
+  var searchTerm = req.params.searchTerm
+  var apiUrl = 'https://api.giphy.com/v1/gifs/search'
+  var apiKey = process.env.GIPHY_API_KEY
+  var requestUrl = `${apiUrl}?api_key=${apiKey}&q=${searchTerm}`
+
+  request.get(requestUrl, function(err, response, body) {
+    console.log(body)
+  })
+})
+
 
 app.listen(port, function(err) {
   console.log(err || `Server running on ${port}.`)
--- ajax_giphy_starter/public/js/app.js
+++ ajax_giphy_starter/public/js/app.js
@@ -1,1 +1,16 @@
 console.log("app.js is loaded in the client...")
+var searchInput = $('#search-input')
+var searchButton = $('#search-button')
+
+
+searchButton.on('click', function() {
+  console.log('click');
+  var requestSettings = {
+    method: 'get',
+    url: '/search/' + searchInput.val()
+  }
+  function cb(d){
+    console.log(d);
+  }
+  $.ajax(requestSettings).done(cb)
+})
