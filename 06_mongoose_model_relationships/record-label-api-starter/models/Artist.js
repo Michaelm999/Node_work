794eca97c88902e074e8a06652fc66f6d3528229
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-14 12:11:46 -0700

    Artist

--- 06_mongoose_model_relationships/record-label-api-starter/models/Artist.js
+++ 06_mongoose_model_relationships/record-label-api-starter/models/Artist.js
@@ -1,1 +1,10 @@
-// Artist model here...
+const mongoose = require('mongoose')
+
+const artistSchema = new mongoose.Schema({
+  name: String,
+  album: [{type: mongoose.Schema.Types.ObjectId, ref: 'Album'}]
+})
+
+const Artist = mongoose.model('Artist', artistSchema)
+
+module.exports = Artist
